cfd6aa85b17ce155b6a25bf0407cbd3be5b02765
Mustafa <mustafaomousa@icloud.com>
2020-10-01 18:28:51 -0500

    Completed adding images

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,19 @@
 window.addEventListener('DOMContentLoaded', event => {
     const board = document.getElementById('tic-tac-toe-board')
+    let clicks = 1;
     board.addEventListener('click', event => {
-
-        event.target.innerHTML = ("<img src=/home/jake/app-Academy/dom-api-tic-tac-toe/player-o.svg>");
+        
+        if (event.target.id.includes('square')) {
+            if (clicks % 2 !== 0) {
+                event.target.innerHTML = ("<img src=/player-x.svg>");
+                clicks++;
+           } else {
+                event.target.innerHTML = ("<img src=/player-o.svg>");
+                clicks++;
+            }
+        }
+        
     })
-
 
 
 });
