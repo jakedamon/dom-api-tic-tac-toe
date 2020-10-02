fa0ac5cb7de8b11289e9d600555ac3c95032a89a
Mustafa <mustafaomousa@icloud.com>
2020-10-02 14:29:33 -0500

    Re-did Player Clicks

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,19 +1,44 @@
+let squareValues = ['','','','','','','','',''];
+let currentPlayerSymbol = 'x'
 window.addEventListener('DOMContentLoaded', event => {
     const board = document.getElementById('tic-tac-toe-board')
-    let clicks = 1;
+
+    
     board.addEventListener('click', event => {
         
-        if (event.target.id.includes('square')) {
-            if (clicks % 2 !== 0) {
-                event.target.innerHTML = ("<img src=/player-x.svg>");
-                clicks++;
-           } else {
-                event.target.innerHTML = ("<img src=/player-o.svg>");
-                clicks++;
-            }
+        const targetId = event.target.id;
+
+        if (!targetId.startsWith('square-')) return;
+
+        const squareIndex = Number.parseInt(targetId[targetId.length-1]);
+
+        if (squareValues[squareIndex] !== '') return;
+
+        const image = document.createElement('img');
+        image.src = `https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${currentPlayerSymbol}.svg`
+        event.target.appendChild(image);
+
+        squareValues[squareIndex] = currentPlayerSymbol;
+
+        if (currentPlayerSymbol === 'x') {
+            currentPlayerSymbol = 'o';
+        } else {
+            currentPlayerSymbol = 'x';
         }
-        
     })
 
+    let checkGameStatus = () => {
+        const x = '<img src="/player-x.svg">'
+        const o = '<img src="/player-o.svg">'
+        
+        for(let i = 0; i < 9; i++){
+            if (event.target.innerHTML == x){
+            playingBoard[i] = x;
+            } else if (event.target.innerHTML == o) {
+                playingBoard[i] = o;
+            }
+        }
+        console.log(playingBoard)
+    }
 
 });
