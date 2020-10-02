736d16f3c80f4c0910100937039e9b8c03a66052
Jake Damon <jacob.damon012@gmail.com>
2020-10-02 17:14:50 -0400

    wokring on requirement 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,44 +1,44 @@
-let squareValues = ['','','','','','','','',''];
-let currentPlayerSymbol = 'x'
-window.addEventListener('DOMContentLoaded', event => {
-    const board = document.getElementById('tic-tac-toe-board')
+let squareValues = ["", "", "", "", "", "", "", "", ""];
+let currentPlayerSymbol = "x";
+let gameStatus = "";
+window.addEventListener("DOMContentLoaded", (event) => {
+  const board = document.getElementById("tic-tac-toe-board");
 
-    
-    board.addEventListener('click', event => {
-        
-        const targetId = event.target.id;
+  board.addEventListener("click", (event) => {
+    const targetId = event.target.id;
 
-        if (!targetId.startsWith('square-')) return;
+    if (!targetId.startsWith("square-")) return;
 
-        const squareIndex = Number.parseInt(targetId[targetId.length-1]);
+    const squareIndex = Number.parseInt(targetId[targetId.length - 1]);
 
-        if (squareValues[squareIndex] !== '') return;
+    if (squareValues[squareIndex] !== "") return;
 
-        const image = document.createElement('img');
-        image.src = `https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${currentPlayerSymbol}.svg`
-        event.target.appendChild(image);
+    const image = document.createElement("img");
+    image.src = `https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${currentPlayerSymbol}.svg`;
+    event.target.appendChild(image);
 
-        squareValues[squareIndex] = currentPlayerSymbol;
+    squareValues[squareIndex] = currentPlayerSymbol;
 
-        if (currentPlayerSymbol === 'x') {
-            currentPlayerSymbol = 'o';
-        } else {
-            currentPlayerSymbol = 'x';
-        }
-    })
-
-    let checkGameStatus = () => {
-        const x = '<img src="/player-x.svg">'
-        const o = '<img src="/player-o.svg">'
-        
-        for(let i = 0; i < 9; i++){
-            if (event.target.innerHTML == x){
-            playingBoard[i] = x;
-            } else if (event.target.innerHTML == o) {
-                playingBoard[i] = o;
-            }
-        }
-        console.log(playingBoard)
+    if (currentPlayerSymbol === "x") {
+      currentPlayerSymbol = "o";
+    } else {
+      currentPlayerSymbol = "x";
     }
+    checkGameStatus();
+  });
 
+  let checkGameStatus = () => {
+    for (let i = 0; i < 9; i += 3) {
+      if (
+        squareValues[i] &&
+        squareValues[i + 1] &&
+        squareValues[i + 2] == squareValues[i]
+      ) {
+        if (gameStatus == "") {
+          gameStatus = squareValues[i].toUpperCase();
+          console.log(gameStatus);
+        }
+      }
+    }
+  };
 });
